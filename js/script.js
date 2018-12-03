47e3909bc62206badf7af402a2a7d47b30f45761
susanatg <susanatgarcia@gmail.com>
2018-12-03 11:56:59 +0000

    Progress on blog

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Carousel
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
